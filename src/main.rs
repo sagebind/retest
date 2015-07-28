e3654eb2303a7bfbe980d75547e4136e4cc06974
coderstephen <me@stephencoakley.com>
2015-07-28 16:59:38 -0500

    First working version

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate getopts;
+extern crate regex;
+extern crate term;
+mod retest;
+
 use getopts::Options;
 use std::env;
 use std::io;
 use std::io::Read;
-mod retest;
 
 /// Prints the program usage to the console.
 fn print_usage(options: Options) {
@@ -20,24 +23,36 @@ fn main() {
 
     let mut options = Options::new();
     options.optflag("h", "help", "Print this help menu");
+    options.optopt("s", "subject", "Specify a subject to match against", "TEXT");
 
     let matches = match options.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
     };
 
+    // If the help flag is present or the user forgot to specify a pattern, show
+    // the usage message.
     if matches.opt_present("h") || matches.free.is_empty() {
         print_usage(options);
         return;
     }
 
-    let regex = matches.free[0].clone();
+    // Get the regular expression pattern from the argument list.
+    let pattern = matches.free[0].clone();
 
+    // Get the subject to test on. If the -s option is present, get the subject
+    // from the argument list, otherwise get the subject from stdin.
     // Get the input text from stdin.
-    let mut input = String::new();
-    let mut stdin = io::stdin();
-    stdin.read_to_string(&mut input).unwrap();
+    let mut subject: String;
 
-    // Test
-    retest::test(&regex, &input);
+    if matches.opt_present("s") {
+        subject = matches.opt_str("s").unwrap();
+    } else {
+        subject = String::new();
+        let mut stdin = io::stdin();
+        stdin.read_to_string(&mut subject).unwrap();
+    }
+
+    // Print out the highlighted subject.
+    retest::print_matches(&pattern, &subject);
 }
--- src/retest.rs
+++ src/retest.rs
@@ -1,6 +1,35 @@
-extern crate regex;
+use regex::Regex;
+use term;
 
-pub fn test(pattern: &str, subject: &str) {
-    let re = regex::Regex::new(&pattern).unwrap();
-    assert!(re.is_match(&subject));
+pub fn print_matches(pattern: &str, subject: &str) {
+    // Attempt to compile the given regex pattern.
+    let regex = match Regex::new(&pattern) {
+        Ok(result) => { result },
+        Err(_) => {
+            println!("Invalid regular expression pattern '{}'.", pattern);
+            return;
+        }
+    };
+
+    // Grab a terminal object to write formatted output with.
+    let mut term = term::stdout().unwrap();
+
+    // Loop over each match in the subject and pretty-print the match as well as
+    // any trailing, non-matching text preceding the match.
+    let mut last_index = 0;
+    for captures in regex.captures_iter(subject) {
+        let positions = captures.pos(0).unwrap();
+
+        print!("{}", &subject[last_index .. positions.0]);
+        term.bg(term::color::BLUE).unwrap();
+        print!("{}", captures.at(0).unwrap());
+        term.reset().unwrap();
+
+        last_index = positions.1;
+    }
+
+    // Print trailing unmatched text if there is any.
+    if last_index < subject.len() {
+        print!("{}", &subject[last_index ..]);
+    }
 }
