1e174c0e4b83af301f62a5f3c6b110a8490778ce
coderstephen <me@stephencoakley.com>
2015-07-28 17:22:48 -0500

    Separate print functions

--- src/retest.rs
+++ src/retest.rs
@@ -1,6 +1,8 @@
-use regex::Regex;
-use term;
+use regex::{Captures, Regex};
+use term::{self, Terminal};
 
+/// Finds all matches for the given pattern on the given subject and prints out
+/// the subject with matches highlighted.
 pub fn print_matches(pattern: &str, subject: &str) {
     // Attempt to compile the given regex pattern.
     let regex = match Regex::new(&pattern) {
@@ -11,9 +13,6 @@ pub fn print_matches(pattern: &str, subject: &str) {
         }
     };
 
-    // Grab a terminal object to write formatted output with.
-    let mut term = term::stdout().unwrap();
-
     // Loop over each match in the subject and pretty-print the match as well as
     // any trailing, non-matching text preceding the match.
     let mut last_index = 0;
@@ -21,10 +20,7 @@ pub fn print_matches(pattern: &str, subject: &str) {
         let positions = captures.pos(0).unwrap();
 
         print!("{}", &subject[last_index .. positions.0]);
-        term.bg(term::color::BLUE).unwrap();
-        print!("{}", captures.at(0).unwrap());
-        term.reset().unwrap();
-
+        print_captures(captures);
         last_index = positions.1;
     }
 
@@ -32,4 +28,14 @@ pub fn print_matches(pattern: &str, subject: &str) {
     if last_index < subject.len() {
         print!("{}", &subject[last_index ..]);
     }
+}
+
+/// Prints out a capture group using color formatting.
+fn print_captures(captures: Captures) {
+    // Grab a terminal object to write formatted output with.
+    let mut terminal = term::stdout().unwrap();
+
+    terminal.bg(term::color::BLUE).unwrap();
+    print!("{}", captures.at(0).unwrap());
+    terminal.reset().unwrap();
 }
